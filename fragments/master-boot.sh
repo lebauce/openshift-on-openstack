226ea3d355cb96e4501b4d65a0dd5db4a8aa94e0
Sylvain Baubeau <sbaubeau@redhat.com>
2015-10-26 00:24:25 +0100

    Restart openshift services

--- fragments/master-boot.sh
+++ fragments/master-boot.sh
@@ -10,4 +10,25 @@ ifup eth1
 sed -i 's/search openstacklocal/&\nnameserver $DNS_IP/' /etc/resolv.conf
 sed -i -e 's/^PEERDNS.*/PEERDNS="no"/' /etc/sysconfig/network-scripts/ifcfg-eth0
 
+# Restart openshift services on master
+service flanneld restart || true
+service docker restart || true
+service atomic-openshift-node restart || true
+iptables -F
+
+# Enable external connectivity
+iptables -I OS_FIREWALL_ALLOW -p tcp -m tcp --dport 1936 -j ACCEPT
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
+# Restart openshift services on nodes
+ssh -o StrictHostKeyChecking=no cloud-user@openshift-node-ooxey.example.com sudo service flanneld restart || true
+ssh -o StrictHostKeyChecking=no cloud-user@openshift-node-ooxey.example.com sudo service docker restart || true
+ssh -o StrictHostKeyChecking=no cloud-user@openshift-node-ooxey.example.com sudo service atomic-openshift-node restart || true
+ssh -o StrictHostKeyChecking=no cloud-user@openshift-node-ooxey.example.com sudo iptables -F || true
+
+ssh -o StrictHostKeyChecking=no cloud-user@openshift-node-tfykn.example.com sudo service flanneld restart || true
+ssh -o StrictHostKeyChecking=no cloud-user@openshift-node-tfykn.example.com sudo service docker restart || true
+ssh -o StrictHostKeyChecking=no cloud-user@openshift-node-tfykn.example.com sudo service atomic-openshift-node restart || true
+ssh -o StrictHostKeyChecking=no cloud-user@openshift-node-tfykn.example.com sudo iptables -F || true
+
 echo "OpenShift has been installed."
