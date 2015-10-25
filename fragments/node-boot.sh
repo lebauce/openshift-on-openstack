5ea3dcb667073ad45592838620ceadc92695bf89
Sylvain Baubeau <sbaubeau@redhat.com>
2015-10-26 00:03:58 +0100

    Do not install openshift on startup

--- fragments/node-boot.sh
+++ fragments/node-boot.sh
@@ -11,10 +11,3 @@ ifup eth1
 sed -i 's/search openstacklocal/&\nnameserver $DNS_IP/' /etc/resolv.conf
 sed -i -e 's/^PEERDNS.*/PEERDNS="no"/' /etc/sysconfig/network-scripts/ifcfg-eth0
 
-
-curl -O http://buildvm-devops.usersys.redhat.com/puddle/build/AtomicOpenShift/3.1/latest/RH7-RHAOS-3.1.repo
-mv RH7-RHAOS-3.1.repo /etc/yum.repos.d/
-
-# master and nodes
-retry yum install -y deltarpm
-retry yum -y update
